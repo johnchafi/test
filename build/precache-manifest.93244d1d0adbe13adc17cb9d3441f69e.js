d36d90b2be9ecb6c65aa58beb93e16ccbbecdca6
Jean de Dieu Uwimana <johnchafi@gmail.com>
2020-12-26 21:45:03 -0500

    first

---
+++ build/precache-manifest.93244d1d0adbe13adc17cb9d3441f69e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b5439925824cb419dcd2d326ffa712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b79ca608d5f04b14d8f",
+    "url": "/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "a08106e9b06c9ea9d32c",
+    "url": "/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "9b79ca608d5f04b14d8f",
+    "url": "/static/js/2.02b502c9.chunk.js"
+  },
+  {
+    "revision": "12d1d34f904ec08b7c978c56e86a7af2",
+    "url": "/static/js/2.02b502c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08106e9b06c9ea9d32c",
+    "url": "/static/js/main.0487ede2.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "2ca1bbb42e6f4f0e3941356ec8e8dbb8",
+    "url": "/static/media/person1.2ca1bbb4.jpg"
+  }
+]);
